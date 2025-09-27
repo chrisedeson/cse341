70e6d5732d9e7da54f27fbe195f24fe1698d4a4b
chrisedeson <edesonchristopher@gmail.com>
2025-09-27 10:15:58 +0100

    Fix duplicate swaggerDocument declaration causing deployment error

--- library-management-api/server.js
+++ library-management-api/server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
 
 // Load environment variables
 dotenv.config();
